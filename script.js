51402e30ef0af895b382f91818a8b12e31cee0a0
QuentinGP <quentgeoffpro@yahoo.com>
2022-12-08 15:27:55 +0100

    popup debut

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
-gsap.registerPlugin(ScrollTrigger);
-gsap.to(".snap-element", {
-  scrollTrigger: {
-    scroller: "snap",
-    trigger: ".snap-element",
-    start: 'center 55%',
-    markers: true,
-    toggleActions: 'play complete restart reverse',
-  }, 
-  scale: 4,
-  duration: 2,
-  ease: 'back',
-});
+// gsap.registerPlugin(ScrollTrigger);
+
+// let sections = gsap.utils.toArray(".snap-element");
+
+// gsap.to(sections, {
+//   xPercent: -100 * (sections.length - 1),
+//   ease: "none",
+//   scrollTrigger: {
+//     trigger: ".snap",
+//     pin: true,
+//     scrub: 1,
+//     snap: {
+//       snapTo: 1 / (sections.length - 1),
+//       duration: 0.1,
+//       delay: 0.1,
+//       ease: "power1.inOut"
+//     },
+//     // base vertical scrolling on how wide the container is so it feels more natural.
+//     end: "+=3500"
+//   }
+// });
