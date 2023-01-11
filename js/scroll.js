b1c1e2b854273c122db01537722b8416ded654ec
QuentinGP <quentgeoffpro@yahoo.com>
2023-01-11 10:54:00 +0100

    .

--- js/scroll.js
+++ js/scroll.js
@@ -7,6 +7,7 @@ const sectionPosition = document.querySelectorAll('.section-position');
 let panelToScroll = 0;
 let currentPanel = 0;
 let currentSectionPosition = 0;
+console.log(sectionPosition);
 
 panelContainer.addEventListener('wheel', (e) => {
 	e.preventDefault();
@@ -14,15 +15,15 @@ panelContainer.addEventListener('wheel', (e) => {
 	if (e.deltaY > 0 && currentPanel < panel.length - 1) {
 		panelToScroll = panelToScroll + clientHeight;
 		currentPanel++;
-		sectionPosition[currentSectionPosition].style.backgroundColor = 'purple';
+		sectionPosition[currentSectionPosition].style.backgroundColor = "white";
 		currentSectionPosition++;
-		sectionPosition[currentSectionPosition].style.backgroundColor = 'red';
+		sectionPosition[currentSectionPosition].style.backgroundColor = "#484141";
 	} else if (e.deltaY < 0 && currentPanel != 0) {
 		panelToScroll = panelToScroll - clientHeight;
 		currentPanel--;
-		sectionPosition[currentSectionPosition].style.backgroundColor = 'purple';
+		sectionPosition[currentSectionPosition].style.backgroundColor = "white";
 		currentSectionPosition--;
-		sectionPosition[currentSectionPosition].style.backgroundColor = 'red';
+		sectionPosition[currentSectionPosition].style.backgroundColor = "#484141";
 	}
 
 	scrollTo({
