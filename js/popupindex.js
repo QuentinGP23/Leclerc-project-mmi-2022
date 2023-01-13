3146c65ceddfb1644335466e8c5f53b7f6f6266c
QuentinGP23 <quentgeoffpro@yahoo.com>
2023-01-13 16:47:01 +0100

    .

--- js/popupindex.js
+++ js/popupindex.js
@@ -1,45 +1,69 @@
 //index
 const biscuit = document.getElementById("biscuit");
 const popupbiscuit = document.getElementById("popupsable");
-
-biscuit.addEventListener('click', () => {
-    popupbiscuit.style.display="block";
-});
-
-// popupbiscuit.addEventListener('mouseleave', () => {
-//     popupbiscuit.style.display="none";
-// });
-
 const popuptartine = document.getElementById("popuptartine");
 const tartine = document.getElementById("tartine");
-
-tartine.addEventListener('click', () => {
-    popuptartine.style.display="block";
-});
-
-// popuptartine.addEventListener('mouseleave', () => {
-//     console.log("ntm");
-//     popuptartine.style.display="none";
-// });
-
 const popupflocon = document.getElementById("popupflocon");
 const flocon = document.getElementById("flocon");
-
-flocon.addEventListener('click', () => {
-    popupflocon.style.display="block";
-});
-
-// popupflocon.addEventListener('mouseleave', () => {
-//     popupflocon.style.display="none";
-// });
-
 const popuptruffe = document.getElementById("popuptruffe");
 const truffe = document.getElementById("truffe");
+const fenetre = document.getElementById("close");
+const livre = document.getElementById("livre");
+const popuplivre = document.getElementById("popuplivre");
 
-truffe.addEventListener('click', () => {
-    popuptruffe.style.display="block";
+biscuit.addEventListener('click', function(event){
+    popupbiscuit.style.display="block";
+    fenetre.style.display="block";
 });
 
-// popuptruffe.addEventListener('mouseleave', () => {
-//     popuptruffe.style.display="none";
-// });
+fenetre.addEventListener('click', function(event){
+    popupbiscuit.style.display="none";
+    fenetre.style.display="none";
+});
+
+tartine.addEventListener('click', function(event){
+    popuptartine.style.display="block";
+    fenetre.style.display="block";
+});
+
+fenetre.addEventListener('click', function(event){
+    popuptartine.style.display="none";
+    fenetre.style.display="none";
+});
+
+flocon.addEventListener('click', function(event){
+    popupflocon.style.display="block";
+    fenetre.style.display="block";
+});
+
+fenetre.addEventListener('click', function(event){
+    popupflocon.style.display="none";
+    fenetre.style.display="none";
+});
+
+truffe.addEventListener('click', function(event){
+    popuptruffe.style.display="block";
+    fenetre.style.display="block";
+});
+
+fenetre.addEventListener('click', function(event){
+    popuptruffe.style.display="none";
+    fenetre.style.display="none";
+});
+
+livre.addEventListener('mouseenter', function(event){
+    livre.setAttribute('src', '/img/livreeffect.svg');
+});
+livre.addEventListener('mouseleave', function(event){
+    livre.setAttribute('src', '/img/livre.svg');
+});
+
+livre.addEventListener('click', function(event){
+    popuplivre.style.display="block";
+    fenetre.style.display="block";
+});
+
+fenetre.addEventListener('click', function(event){
+    popuplivre.style.display="none";
+    fenetre.style.display="none";
+});
