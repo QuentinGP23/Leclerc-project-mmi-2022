8f49ea967ee0ba0f6b6415cb9d04b68cae9d66d2
QuentinGP <quentgeoffpro@yahoo.com>
2023-01-15 00:06:50 +0100

    .

--- js/header.js
+++ js/header.js
@@ -1,11 +1,16 @@
 const openmenu = document.querySelector('#openmenu');
 const headershow = document.querySelector('.headershow');
 const navcroix = document.querySelector("#navcroix")
+const body = document.body;
 //header
+console.log("gello");
 openmenu.addEventListener('click', () => {
+    console.log("wtf");
     headershow.classList.add('headershow1');
+    body.style.overflow="hidden";
 });
 
 navcroix.addEventListener('click', () => {
     headershow.classList.remove('headershow1');
+    body.style.overflow="visible";
 });
